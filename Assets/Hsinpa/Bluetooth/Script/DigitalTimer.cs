1e3ad8a9a2428413b991376ee1d051e841692eae
Hsinpa <hsinpa@gmail.com>
2022-07-02 23:11:35 +0800

    Add timer base script

---
+++ Assets/Hsinpa/Bluetooth/Script/DigitalTimer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Hsinpa.Bluetooth
+{
+    public class DigitalTimer
+    {
+        private System.DateTime start_datetime;
+        private System.TimeSpan leak_datetime;
+
+        private bool timer_state = false;
+        public bool TimerState => timer_state;
+
+        public void StartTimer() {
+            start_datetime = System.DateTime.UtcNow;
+
+            timer_state = true;
+        }
+
+        public void StopTimer() {
+            timer_state = false;
+
+            leak_datetime = GetTimeDifferent();
+        }
+
+        public System.Int32 GetHour()
+        {
+            return GetTimeDifferent().Hours;
+        }
+
+        public System.Int32 GetMinute()
+        {
+            return GetTimeDifferent().Minutes;
+        }
+
+        public System.Int32 GetSecond()
+        {
+            return GetTimeDifferent().Seconds;
+        }
+
+        private System.TimeSpan GetTimeDifferent()
+        {
+            return (System.DateTime.UtcNow - start_datetime) + leak_datetime;
+        }
+
+        public void Dispose() {
+            start_datetime = System.DateTime.MinValue;
+            leak_datetime = System.TimeSpan.Zero;
+            timer_state = false;
+        }
+    }
+}
